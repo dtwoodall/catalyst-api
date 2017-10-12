86b5d0ccc3ca26fc9fcf8edebe06abbd130a92ed
Daniel Woodall <dtwoodall@gmail.com>
2017-10-11 17:46:58 -0700

    Initial server deploy after client-server separation

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  API_AUDIENCE_ATTRIBUTE: process.env.API_AUDIENCE_ATTRIBUTE
+};
---
+++ index.js
@@ -0,0 +1,219 @@
+const express = require('express');
+const cors = require('cors');
+const Sequelize = require('sequelize');
+const path = require('path');
+const bodyParser = require('body-parser');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const config = require('./config/config');
+
+// Auth0 Authentication middleware
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: `${config.API_AUDIENCE_ATTRIBUTE}`,
+  issuer: `https://${config.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+// Database setup
+const sequelize = new Sequelize('postgres://postgres:Pg11235!@localhost:5432/scheduler');
+
+const formats = {
+  COLOR: {
+    is: /^#([0-9A-F]{3}|[0-9A-F]{6})$/
+  },
+  STATUS: {
+    in: ['Not Started', 'In Progress']
+  }
+}
+
+const Category = sequelize.define('category', {
+  name: Sequelize.STRING,
+  color: {
+    type: Sequelize.STRING,
+    validate: formats.COLOR
+  }
+});
+
+const Task = sequelize.define('task', {
+  summary: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  status: {
+    type: Sequelize.STRING,
+    validate: formats.STATUS
+  }
+});
+
+Task.belongsTo(Category);
+Category.hasMany(Task);
+
+Task.belongsTo(Task, {as: 'parent'});
+Task.hasMany(Task, {as: 'subtasks', foreignKey: 'parentId'});
+
+sequelize.sync();
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.log(`Unable to connect to the database:  ${err}`);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+app.options('*', cors());
+
+app.get('/tasks', authCheck, (req, res) => {
+
+  Task.findAll({
+    attributes: ['id', 'summary', 'description', 'categoryId', 'parentId'],
+    include: [{
+      model: Category,
+      attributes: ['id', 'name', 'color']
+    }]
+  }).then(tasks => res.json(tasks));
+
+});
+
+app.get('/tasks/:taskId', authCheck, (req, res) => {
+
+  Task.findOne({
+    attributes: ['id', 'summary', 'description', 'categoryId', 'parentId'],
+    where: {
+      id: req.params.taskId
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'name', 'color']
+      }, {
+        model: Task,
+        as: 'subtasks',
+        attributes: ['id', 'summary', 'description', 'parentId']
+      }
+    ]
+  }).then(task => res.json(task));
+
+});
+
+app.post('/tasks', authCheck, (req, res) => {
+
+  const {summary, description, categoryId, parentId} = req.body;
+
+  Task.create({summary, description, categoryId, parentId}).then(newTask => {
+    return res.json(newTask);
+  }).catch(err => console.log(err));
+
+});
+
+app.post('/tasks/:taskId', authCheck, (req, res) => {
+
+  const {summary, description, categoryId} = req.body;
+
+  return Task.findOne({
+    attributes: ['id', 'summary', 'description', 'categoryId', 'parentId'],
+    where: {
+      id: req.params.taskId
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'name', 'color']
+      }, {
+        model: Task,
+        as: 'subtasks',
+        attributes: ['id', 'summary', 'description', 'parentId']
+      }
+    ]
+  }).then(task => {
+    return task.update({summary, description, categoryId}).then(updatedTask => {
+      return res.json(updatedTask);
+    }).catch(err => console.log(err));
+  }).catch(err => console.log(err));
+
+});
+
+app.get('/categories', authCheck, (req, res) => {
+
+  Category.findAll({
+    attributes: ['id', 'name', 'color']
+  }).then(categories => res.json(categories));
+
+});
+
+app.get('/categories/:categoryId', authCheck, (req, res) => {
+
+  Category.findOne({
+    attributes: ['id', 'name', 'color'],
+    where: {
+      id: req.params.categoryId
+    }
+  }).then(category => res.json(category));
+
+});
+
+app.get('/categories/:categoryId/tasks', authCheck, (req, res) => {
+
+  Task.findAll({
+    attributes: ['id', 'summary', 'description', 'parentId', 'categoryId'],
+    where: {
+      parentId: null,
+      categoryId: req.params.categoryId
+    }
+  }).then(tasks => res.json(tasks));
+
+});
+
+app.post('/categories', authCheck, (req, res) => {
+
+  const {name, color} = req.body;
+
+  Category.create({name, color}).then(newCategory => {
+    return res.json(newCategory);
+  }).catch(err => console.log(err));
+
+});
+
+app.post('/categories/:categoryId', authCheck, (req, res) => {
+
+  const {name, color} = req.body;
+
+  return Category.findOne({
+    attributes: ['id', 'name', 'color'],
+    where: {
+      id: req.params.categoryId
+    }
+  }).then(category => {
+    return category.update({name, color}).then(updatedCategory => {
+      return res.json(updatedCategory);
+    }).catch(err => console.log(err));
+  }).catch(err => console.log(err));
+
+});
+
+
+
+app.get('/*', (req, res) => {
+  return res.send('Content not found! Check your url.');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+
+const client = express();
+client.use(express.static('client'));
+client.listen(3000);
+
+console.log(`Scheduler listening on ${port}`);
